9d3f59ba4f88bc1b20050ff0fff065dd292ce74b
RyanideTech <39866457+RyanideTech@users.noreply.github.com>
2018-07-12 15:17:13 -0400

    Add constructor

--- Items.cs
+++ Items.cs
@@ -6,5 +6,19 @@ namespace AdventureGame
 {
     public class Item
     {
+        public string Name { get; set; }
+        public string Effect { get; set; }
+        public string Description { get; set; }
+
+        public Item(string name, string effect, string description)
+        {
+            Name = name;
+            Effect = effect;
+            Description = description;
+
+            //Assign effect method
+        }
+
+
     }
 }
